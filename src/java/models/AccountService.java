8d59c6a3d2ec13f1ca89469c9ec96cc202f47478
Zachary <696019@SAIT221356IT.ACDM.DS.SAIT.CA>
2020-10-08 17:20:11 -0600

    /home now redirects to /login if it is a new session

--- src/java/models/AccountService.java
+++ src/java/models/AccountService.java
@@ -2,11 +2,12 @@ package models;
 
 public class AccountService 
 {
-//    public User login(String username,String password)
-//    {
-//        if (username.equals("adam") || username.equals("betty") && password.equals("password"))
-//        {
-//            
-//        }
-//    }
+    public User login(String username,String password)
+    {
+        if (username.equals("adam") || username.equals("betty") && password.equals("password"))
+        {
+            return new User(username, null);
+        }
+        return null;
+    }
 }
--- src/java/models/User.java
+++ src/java/models/User.java
@@ -1,19 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package models;
 
-/**
- *
- * @author 696019
- */
-public class User 
+import java.io.Serializable;
+
+public class User implements Serializable
 {
     public String username;
     public String password;
 
+    public User() 
+    {
+        
+    }
+
+    
     public User(String username, String password) 
     {
         this.username = username;
